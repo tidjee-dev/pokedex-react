5fa39a2da8c88a240de2191ece5b82f4a944aef7
Donatien Pinet <pinet.donatien@gmail.com>
2024-12-18 11:42:43 +0100

    📦 NEW: Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <div className="grid grid-rows-[5rem_auto_5rem] gap-6 h-screen">
+      <Header />
+      <main className="container flex flex-col items-center justify-center gap-6"></main>
+    </div>
+  );
 }
 
 export default App;
